bfb906f239881183048d67f6eb2ffc4e0a4cdd6a
xtrapp <jefer_son11@hotmail.com>
2019-03-24 16:28:44 -0300

    Scanner

---
+++ src/scaner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class scaner {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite as palavras");
+
+        String leitura = sc.nextLine();
+        String x,y,z;
+        x = sc.next();
+        y = sc.next();
+        z = sc.next();
+
+        System.out.println(leitura);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+
+    }
+
+
+}
